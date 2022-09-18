ddecb1a96a9d080b9ae3691d8f2d6c427737e071
Junwei Hu <jwhu@apac.freewheel.com>
2022-09-19 00:18:16 +0800

    Minor change UI

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -46,34 +46,34 @@ export default function App() {
         <header class="p-3 text-bg-dark">
           <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-              {/* <a
-                    href="/"
-                    class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-                  >
-                    <svg
-                      class="bi me-2"
-                      width="40"
-                      height="32"
-                      role="img"
-                      aria-label="Bootstrap"
-                    >
-                      <use xlink:href="#bootstrap" />
-                    </svg>
-                  </a> */}
+              <a
+                href="/"
+                class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+              >
+                <svg
+                  class="bi me-2"
+                  width="40"
+                  height="32"
+                  role="img"
+                  aria-label="Bootstrap"
+                >
+                  
+                </svg>
+              </a>
 
               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li>
                   <a
-                    href="/"
+                    href="javascript:void(0);"
                     onClick={getPeople}
-                    class="nav-link px-2 text-secondary"
+                    class="nav-link px-2 text-white"
                   >
                     Get from server
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="javascript:void(0);"
                     onClick={() => setShowingCreateNewPersonForm(true)}
                     class="nav-link px-2 text-white"
                   >
@@ -82,7 +82,7 @@ export default function App() {
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="javascript:void(0);"
                     onClick={() => setPeople([])}
                     class="nav-link px-2 text-white"
                   >
