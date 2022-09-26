6a4ac8621a016bb558abc95b58d3f285597951a1
Junwei Hu <jwhu@apac.freewheel.com>
2022-09-26 20:56:24 +0800

    Make spinner to right

--- reactclient/src/layouts/dashboard/components/Projects/index.js
+++ reactclient/src/layouts/dashboard/components/Projects/index.js
@@ -17,9 +17,6 @@ import { useEffect, useState } from "react";
 
 // @mui material components
 import Card from "@mui/material/Card";
-import Icon from "@mui/material/Icon";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 
 import MDSpinner from "react-md-spinner";
 
@@ -37,32 +34,6 @@ import useToken from "utilities/UseToken";
 import useErrorHandler from "utilities/useErrorHandler";
 
 function Projects() {
-  const [menu, setMenu] = useState(null);
-
-  const openMenu = ({ currentTarget }) => setMenu(currentTarget);
-  const closeMenu = () => setMenu(null);
-
-  const renderMenu = (
-    <Menu
-      id="simple-menu"
-      anchorEl={menu}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={Boolean(menu)}
-      onClose={closeMenu}
-    >
-      <MenuItem onClick={closeMenu}>Action</MenuItem>
-      <MenuItem onClick={closeMenu}>Another action</MenuItem>
-      <MenuItem onClick={closeMenu}>Something else</MenuItem>
-    </Menu>
-  );
-
   const columns = [
     { Header: "name", accessor: "name", width: "40%", align: "left" },
     { Header: "alias", accessor: "alias", width: "20%", align: "left" },
@@ -134,15 +105,12 @@ function Projects() {
       <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
         <MDBox>
           <MDTypography variant="h6" gutterBottom>
-            Official alias links @ FREEWHEEL {loading && <MDSpinner aria-label="Loading..." />}
+            Official alias links @ FREEWHEEL
           </MDTypography>
         </MDBox>
         <MDBox color="text" px={2}>
-          <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
-            more_vert
-          </Icon>
-        </MDBox>
-        {renderMenu}
+          {loading && <MDSpinner aria-label="Loading..." />}
+        </MDBox> 
       </MDBox> 
       {loadOfficialUrlsError && renderAlert()}
       <MDBox display="flex" alignItems="center">
