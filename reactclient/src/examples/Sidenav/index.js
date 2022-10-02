523cbac7acb4e3907667e6832dd606d5578c9879
Junwei Hu <jwhu@apac.freewheel.com>
2022-10-02 17:16:59 +0800

    hide sign-in/sign-out

--- reactclient/src/examples/Sidenav/index.js
+++ reactclient/src/examples/Sidenav/index.js
@@ -85,7 +85,9 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
   }, [dispatch, location]);
 
   // Render all the routes from the routes.js (All the visible items on the Sidenav)
-  const renderRoutes = routes.map(({ type, name, icon, title, noCollapse, key, href, route }) => {
+  const renderRoutes = routes
+  .filter(r => r.route !== "/authentication/sign-in" && r.route !== "/authentication/sign-up")
+  .map(({ type, name, icon, title, noCollapse, key, href, route }) => {
     let returnValue;
 
     if (type === "collapse") {
