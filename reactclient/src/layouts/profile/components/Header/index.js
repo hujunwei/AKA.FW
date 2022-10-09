1986ae4b205431b8fd72f6929231f1b14e52374c
Junwei Hu <jwhu@apac.freewheel.com>
2022-10-09 23:30:27 +0800

    Add link to redirector json

--- reactclient/src/layouts/profile/components/Header/index.js
+++ reactclient/src/layouts/profile/components/Header/index.js
@@ -121,7 +121,7 @@ function Header() {
         </MDTypography>
 
         <MDTypography variant="body2" fontWeight="regular">
-          <b>Step2</b> Import <a href="www.google.com">Aka_Fw_Redirector_Config.json</a> in the
+          <b>Step2</b> Import <a href="https://wiki.freewheel.tv/display/OB/Aka_Fw_Redirector_Config.json">Aka_Fw_Redirector_Config.json</a> in the
           extension.
           <br />
         </MDTypography>
